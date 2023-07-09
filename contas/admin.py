799711fe7db6a1c6590113211d0c45892650b6ad
Gil Santos <gilteksys21@gmail.com>
2023-07-09 13:34:19 -0300

    aula 3

---
+++ contas/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from contas.models import ContaPagar, ContaPaga
+
+admin.site.register(ContaPagar)
+admin.site.register(ContaPaga)
--- perfil/views.py
+++ perfil/views.py
@@ -1,20 +1,34 @@
+from datetime import datetime
 from django.shortcuts import render, redirect
 from django.contrib.messages import constants
 from django.contrib import messages
-from perfil.utils import calcula_total  
+from extrato.models import Valores
+from perfil.utils import calcula_equilibrio_financeiro, calcula_total
 from .models import Conta, Categoria
-
+from django.db.models import Sum
 
 
 def home(request):
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+    entradas = valores.filter(tipo='E')
+    saidas = valores.filter(tipo='S')
+
+    total_entradas = calcula_total(entradas, 'valor')
+    total_saidas = calcula_total(saidas, 'valor')
+
     contas = Conta.objects.all()
     saldo_total = calcula_total(contas, 'valor')
-    return render(request, 'home.html', {'contas': contas, 'saldo_total': saldo_total,})
+
+    percentual_gastos_essenciais, percentual_gastos_nao_essenciais = calcula_equilibrio_financeiro()
+    return render(request, 'home.html', {'contas': contas, 'saldo_total': saldo_total,
+                                         'total_entradas': total_entradas, 'total_saidas': total_saidas, 'percentual_gastos_essenciais': percentual_gastos_essenciais,
+                                         'percentual_gastos_nao_essenciais': percentual_gastos_nao_essenciais})
+
 
 def gerenciar(request):
     contas = Conta.objects.all()
     categorias = Categoria.objects.all()
-    #total_contas = contas.aggregate(Sum('valor'))
+    # total_contas = contas.aggregate(Sum('valor'))
     total_contas = 0
 
     for conta in contas:
@@ -23,19 +37,21 @@ def gerenciar(request):
     print(total_contas)
     return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas, 'categorias': categorias})
 
+
 def cadastrar_banco(request):
     apelido = request.POST.get('apelido')
     banco = request.POST.get('banco')
     tipo = request.POST.get('tipo')
     valor = request.POST.get('valor')
     icone = request.FILES.get('icone')
-    
+
     if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
-        messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+        messages.add_message(request, constants.ERROR,
+                             'Preencha todos os campos')
         return redirect('/perfil/gerenciar/')
-    
+
     conta = Conta(
-        apelido = apelido,
+        apelido=apelido,
         banco=banco,
         tipo=tipo,
         valor=valor,
@@ -43,16 +59,20 @@ def cadastrar_banco(request):
     )
 
     conta.save()
-    messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso !')
+    messages.add_message(request, constants.SUCCESS,
+                         'Conta cadastrada com sucesso !')
 
     return redirect('/perfil/gerenciar/')
+
 
 def deletar_banco(request, id):
     conta = Conta.objects.get(id=id)
     conta.delete()
-    
-    messages.add_message(request, constants.SUCCESS, 'Conta removida com sucesso')
+
+    messages.add_message(request, constants.SUCCESS,
+                         'Conta removida com sucesso')
     return redirect('/perfil/gerenciar/')
+
 
 def cadastrar_categoria(request):
     nome = request.POST.get('categoria')
@@ -65,8 +85,10 @@ def cadastrar_categoria(request):
 
     categoria.save()
 
-    messages.add_message(request, constants.SUCCESS, 'Categoria cadastrada com sucesso')
+    messages.add_message(request, constants.SUCCESS,
+                         'Categoria cadastrada com sucesso')
     return redirect('/perfil/gerenciar/')
+
 
 def update_categoria(request, id):
     categoria = Categoria.objects.get(id=id)
@@ -76,3 +98,14 @@ def update_categoria(request, id):
     categoria.save()
 
     return redirect('/perfil/gerenciar/')
+
+
+def dashboard(request):
+    dados = {}
+    categorias = Categoria.objects.all()
+
+    for categoria in categorias:
+        dados[categoria.categoria] = Valores.objects.filter(
+            categoria=categoria).aggregate(Sum('valor'))['valor__sum']
+
+    return render(request, 'dashboard.html', {'labels': list(dados.keys()), 'values': list(dados.values())})
